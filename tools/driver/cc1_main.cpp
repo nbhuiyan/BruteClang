f732cbd0ca19b017a3601d3ce424bead1ae66cc2
Sarah Nadi <nadi@ualberta.ca>
2017-08-02 10:22:04 -0600

    Fix indentation and move common code outside if

--- tools/driver/cc1_main.cpp
+++ tools/driver/cc1_main.cpp
@@ -173,79 +173,76 @@ int cc1_main(ArrayRef<const char *> Argv, const char *Argv0, void *MainAddr) {
   ensureSufficientStack();
   std::ifstream config;
   config.open("var.config");
-  if (!config){
-  std::unique_ptr<CompilerInstance> Clang(new CompilerInstance());
-  IntrusiveRefCntPtr<DiagnosticIDs> DiagID(new DiagnosticIDs());
-
-  // Register the support for object-file-wrapped Clang modules.
-  auto PCHOps = Clang->getPCHContainerOperations();
-  PCHOps->registerWriter(llvm::make_unique<ObjectFilePCHContainerWriter>());
-  PCHOps->registerReader(llvm::make_unique<ObjectFilePCHContainerReader>());
 
   // Initialize targets first, so that --version shows registered targets.
-  llvm::InitializeAllTargets();
-  llvm::InitializeAllTargetMCs();
-  llvm::InitializeAllAsmPrinters();
-  llvm::InitializeAllAsmParsers();
-
-#ifdef LINK_POLLY_INTO_TOOLS
-  llvm::PassRegistry &Registry = *llvm::PassRegistry::getPassRegistry();
-  polly::initializePollyPasses(Registry);
-#endif
-
-  // Buffer diagnostics from argument parsing so that we can output them using a
-  // well formed diagnostic object.
-  IntrusiveRefCntPtr<DiagnosticOptions> DiagOpts = new DiagnosticOptions();
-  TextDiagnosticBuffer *DiagsBuffer = new TextDiagnosticBuffer;
-  DiagnosticsEngine Diags(DiagID, &*DiagOpts, DiagsBuffer);
-  bool Success = CompilerInvocation::CreateFromArgs(
-      Clang->getInvocation(), Argv.begin(), Argv.end(), Diags);
-
-  // Infer the builtin include path if unspecified.
-  if (Clang->getHeaderSearchOpts().UseBuiltinIncludes &&
-      Clang->getHeaderSearchOpts().ResourceDir.empty())
-    Clang->getHeaderSearchOpts().ResourceDir =
-      CompilerInvocation::GetResourcesPath(Argv0, MainAddr);
-
-  // Create the actual diagnostics engine.
-  Clang->createDiagnostics();
-  if (!Clang->hasDiagnostics())
-    return 1;
-
-  // Set an error handler, so that any LLVM backend diagnostics go through our
-  // error handler.
-  llvm::install_fatal_error_handler(LLVMErrorHandler,
-                                  static_cast<void*>(&Clang->getDiagnostics()));
-
-  DiagsBuffer->FlushDiagnostics(Clang->getDiagnostics());
-  if (!Success)
-    return 1;
-
-  // Execute the frontend actions.
-  Success = ExecuteCompilerInvocation(Clang.get());
-
-  // If any timers were active but haven't been destroyed yet, print their
-  // results now.  This happens in -disable-free mode.
-  llvm::TimerGroup::printAll(llvm::errs());
-
-  // Our error handler depends on the Diagnostics object, which we're
-  // potentially about to delete. Uninstall the handler now so that any
-  // later errors use the default handling behavior instead.
-  llvm::remove_fatal_error_handler();
-
-  // When running with -disable-free, don't do any destruction or shutdown.
-  if (Clang->getFrontendOpts().DisableFree) {
-    BuryPointer(std::move(Clang));
-    return !Success;
-  }
-
-  return !Success;
-  }
-  else{
     llvm::InitializeAllTargets();
     llvm::InitializeAllTargetMCs();
     llvm::InitializeAllAsmPrinters();
     llvm::InitializeAllAsmParsers();
+
+  if (!config){
+    std::unique_ptr<CompilerInstance> Clang(new CompilerInstance());
+    IntrusiveRefCntPtr<DiagnosticIDs> DiagID(new DiagnosticIDs());
+
+    // Register the support for object-file-wrapped Clang modules.
+    auto PCHOps = Clang->getPCHContainerOperations();
+    PCHOps->registerWriter(llvm::make_unique<ObjectFilePCHContainerWriter>());
+    PCHOps->registerReader(llvm::make_unique<ObjectFilePCHContainerReader>());
+
+  #ifdef LINK_POLLY_INTO_TOOLS
+    llvm::PassRegistry &Registry = *llvm::PassRegistry::getPassRegistry();
+    polly::initializePollyPasses(Registry);
+  #endif
+
+    // Buffer diagnostics from argument parsing so that we can output them using a
+    // well formed diagnostic object.
+    IntrusiveRefCntPtr<DiagnosticOptions> DiagOpts = new DiagnosticOptions();
+    TextDiagnosticBuffer *DiagsBuffer = new TextDiagnosticBuffer;
+    DiagnosticsEngine Diags(DiagID, &*DiagOpts, DiagsBuffer);
+    bool Success = CompilerInvocation::CreateFromArgs(
+        Clang->getInvocation(), Argv.begin(), Argv.end(), Diags);
+
+    // Infer the builtin include path if unspecified.
+    if (Clang->getHeaderSearchOpts().UseBuiltinIncludes &&
+        Clang->getHeaderSearchOpts().ResourceDir.empty())
+      Clang->getHeaderSearchOpts().ResourceDir =
+        CompilerInvocation::GetResourcesPath(Argv0, MainAddr);
+
+    // Create the actual diagnostics engine.
+    Clang->createDiagnostics();
+    if (!Clang->hasDiagnostics())
+      return 1;
+
+    // Set an error handler, so that any LLVM backend diagnostics go through our
+    // error handler.
+    llvm::install_fatal_error_handler(LLVMErrorHandler,
+                                    static_cast<void*>(&Clang->getDiagnostics()));
+
+    DiagsBuffer->FlushDiagnostics(Clang->getDiagnostics());
+    if (!Success)
+      return 1;
+
+    // Execute the frontend actions.
+    Success = ExecuteCompilerInvocation(Clang.get());
+
+    // If any timers were active but haven't been destroyed yet, print their
+    // results now.  This happens in -disable-free mode.
+    llvm::TimerGroup::printAll(llvm::errs());
+
+    // Our error handler depends on the Diagnostics object, which we're
+    // potentially about to delete. Uninstall the handler now so that any
+    // later errors use the default handling behavior instead.
+    llvm::remove_fatal_error_handler();
+
+    // When running with -disable-free, don't do any destruction or shutdown.
+    if (Clang->getFrontendOpts().DisableFree) {
+      BuryPointer(std::move(Clang));
+      return !Success;
+    }
+
+    return !Success;
+  } else{
+
     /*std::list<std::string> good_CI; //list of bad compiler instances
     std::list<std::string> bad_CI; //list of good compiler instances*/
     std::string str; //string buffer to store input from config file
